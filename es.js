69cf3639f135305b8f9852af18c8bdd4bb40e72d
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-05-07 15:27:14 +0200

    es 62

--- es.js
+++ es.js
@@ -324,10 +324,15 @@ const message = "JavaScript è fantastico";
 
 // 61) Conta quante volte appare la lettera "a"
 
-const countA = (message.match(/a/g) || []).length;
-console.log(countA);
+// const countA = (message.match(/a/g) || []).length;
+// console.log(countA);
 
 // 62) Trasforma solo la prima lettera in maiuscolo
+
+const firstUpper = message.charAt(0).toUpperCase() + message.slice(1);
+
+console.log(firstUpper);
+
 // 63) Ritorna la stringa come array di caratteri
 // 64) Sostituisci tutte le "a" con "*"
 // 65) Tronca la stringa a 10 caratteri
